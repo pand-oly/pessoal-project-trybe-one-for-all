0cc133f65f35d8f8202abe2533d7cb0fa94a4b0d
pand-oly <hoa98hoa@gmail.com>
2022-07-25 17:40:08 -0300

    Create query for desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT name 'nome_musica',
+    CASE
+      WHEN name LIKE '%Streets%' THEN REPLACE(name, 'Streets', 'Code Review')
+      WHEN name LIKE '%Her Own%' THEN REPLACE(name, 'Her Own', 'Trybe')
+      WHEN name LIKE '%Inner Fire%' THEN REPLACE(name, 'Inner Fire', 'Project')
+      WHEN name LIKE '%Silly%' THEN REPLACE(name, 'Silly', 'Nice')
+      WHEN name LIKE '%Circus%' THEN REPLACE(name, 'Circus', 'Pull Request')
+    END 'novo_nome'
+FROM SpotifyClone.songs
+WHERE song_id in (3, 18, 4, 15, 6);
